46305075229335e02846d496bcfef3f99dd96c23
M. Mahdi Gheysari <67825501+mmgh900@users.noreply.github.com>
2021-12-06 21:33:01 +0330

    Delete peer.py

--- peer.py
+++
@@ -1,9 +0,0 @@
-import requests
-from essential_generators import DocumentGenerator
-
-gen = DocumentGenerator()
-
-for i in range(1, 30):
-    with open(f'./ownedFiles/file{i}', 'w') as file:
-        file.write(f'*** FILE NUMBER {i} ***\n')
-        file.write(gen.sentence())
